ca0e3ab0371d7370b6292216857647b28e240e69
Issam <iba10@pitt.edu>
2022-02-20 14:32:51 -0500

    Starting Ground!

---
+++ Trekr/Trekr/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  Trekr
+//
+//  Created by Issam Abushaban on 2/20/22.
+//
+
+import Foundation
+
+struct Location {
+    let id: Int
+    let name: String
+    let country: String
+    let description: String
+    let latitude: Double
+    let longitude: Double
+    let heroPicture: String
+    let advisory: String
+}
---
+++ Trekr/Trekr/TrekrApp.swift
@@ -0,0 +1,19 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Issam Abushaban on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
